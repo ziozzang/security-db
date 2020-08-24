43e1b4207f27f95b32e00c0226ff97dcae4e2e02
Jioh L. Jung <ziozzang@gmail.com>
2020-08-24 10:28:46 +0900

    Create update-db.sh

---
+++ update-db.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+curl https://pivotal-clamav-mirror.s3.amazonaws.com/main.cvd     > /data/main.cvd
+curl https://pivotal-clamav-mirror.s3.amazonaws.com/daily.cvd    > /data/daily.cvd
+curl https://pivotal-clamav-mirror.s3.amazonaws.com/bytecode.cvd > /data/bytecode.cvd
+
+
+get_latest_release() {
+  curl --silent "https://api.github.com/repos/$1/releases/latest" | # Get latest release from GitHub api
+    grep '"tag_name":' |                                            # Get tag line
+    sed -E 's/.*"([^"]+)".*/\1/'                                    # Pluck JSON value
+}
+
+DB_VERSION=${DB_VERSION:-`get_latest_release aquasecurity/trivy-db`}
+
+curl -L "https://github.com/aquasecurity/trivy-db/releases/download/${DB_VERSION}/trivy-offline.db.tgz" -o /data/trivy-offline.db.tar.gz
+
